7a10493f4ae8dc966118a8e0419558f5d6f50dff
Zack Yang <zk_yang@hotmail.com>
2017-01-26 11:25:52 +1100

    Customized view and template for constitution based on blog

---
+++ routes/views/constitution.js
@@ -0,0 +1,46 @@
+var keystone = require('keystone');
+var async = require('async');
+
+exports = module.exports = function (req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// Init locals
+	locals.section = 'constitution';
+	locals.filters = {
+		category: req.params.category,
+	};
+	locals.data = {
+		posts: [],
+		categories: [],
+	};
+
+	// Load the posts
+	view.on('init', function (next) {
+
+		var q = keystone.list('Post').paginate({
+			page: req.query.page || 1,
+			perPage: 10,
+			maxPages: 10,
+			filters: {
+				state: 'published',
+				notConstitution: 'false',
+			},
+		})
+			.sort('-publishedDate')
+			.populate('author categories');
+
+		if (locals.data.category) {
+			q.where('categories').in([locals.data.category]);
+		}
+
+		q.exec(function (err, results) {
+			locals.data.posts = results;
+			next(err);
+		});
+	});
+
+	// Render the view
+	view.render('constitution');
+};
